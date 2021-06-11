79dced0655adecdade48d3f3fb9665ed032e2949
navidaziz <navidaziz98@gmail.com>
2021-06-11 20:24:50 +0500

    change in recption deletion text

--- application/controllers/admin/lab.php
+++ application/controllers/admin/lab.php
@@ -358,7 +358,7 @@ class Lab extends Admin_Controller
 		$query = "UPDATE invoices
 			          SET is_deleted=1, 
 					  cancel_reason='Faluty entry',  
-					  cancel_reason_detail='Deleted By resection' 
+					  cancel_reason_detail='Deleted By Reception' 
 					  WHERE invoice_id= $invoice_id";
 		if ($this->db->query($query)) {
 			$this->session->set_flashdata("msg_success", "Receipt Cancelled Successfully.");
